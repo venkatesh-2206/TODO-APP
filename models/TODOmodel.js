a5c798c93b239a08a887f67881ba1ac34f92891c
venkatesh-2206 <seenuvasanvenkatesh@gmail.com>
2024-12-11 12:11:00 +0530

    updated schema

--- models/TODOmodel.js
+++ models/TODOmodel.js
@@ -1,7 +1,11 @@
 const mongoose=require("mongoose");
 const todoschema=new mongoose.Schema({
     task:{type:String},
-    status:{type:String}
+    status:{type:String},
+    dueDate: { type: Date },                        
+    createdAt: { type: Date, default: Date.now },    
+    updatedAt: { type: Date, default: Date.now },                       
+    assignedTo: { type: String }
 });
 const TODO= mongoose.model('TODO',todoschema);
 module.exports=TODO;
